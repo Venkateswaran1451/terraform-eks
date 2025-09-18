8a29eccda84614f3024373fe3fb964c76fd584f5
Venkateswaran Easwaramoorthy <53052108+Venkateswaran1451@users.noreply.github.com>
2025-09-18 20:16:32 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,30 +1,32 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.13.3"
+
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.65"
+    }
+
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
+      version = ">= 2.7.1"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.68.0"
-    }
+
     local = {
       source  = "hashicorp/local"
       version = "~> 2.1.0"
     }
+
     null = {
       source  = "hashicorp/null"
       version = "~> 3.1.0"
     }
+
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
-    }
-  }
-}
+      version
