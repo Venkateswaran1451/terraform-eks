f587eb04c3e6f313ab269ac08b536c63802c457a
Venkateswaran Easwaramoorthy <53052108+Venkateswaran1451@users.noreply.github.com>
2025-09-21 18:04:35 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ state = "available"
 }
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "venkates-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "venkates-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
